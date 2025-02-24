06392b6363028a2fad4649f75c3f24ad10a057e7
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-02-24 10:43:24 -0700

    PREP1:
    Updated some variable names to fit standard conventions.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?"); // asks for the person's first name
-        String first_name = Console.ReadLine(); // takes an input for the person's first name
+        String firstName = Console.ReadLine(); // takes an input for the person's first name
         Console.WriteLine("What is your last name?"); // asks for the person's last name
-        String last_name = Console.ReadLine(); // takes an input for the person's last name
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}"); // prints out the person's name
+        String lastName = Console.ReadLine(); // takes an input for the person's last name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}"); // prints out the person's name
     }
 }
