2c103abe435fdcafd1fa4434fcd3e290497869d2
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-02-24 10:26:13 -0700

    Added statements to check where the grade provided by the user falls in the grading scale. Also informs the user if they pass the class or if they need to contact the instructor.
    Simplified the statements to have only one print statement for the grade.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.WriteLine("Enter your Grade: ");
+        String userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+        String letter = "";
+        if (grade >= 90)
+        {
+            //Console.WriteLine($"Your entered grade is {grade}, you got an A.");
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            //Console.WriteLine($"Your entered grade is {grade}, you got a B.");
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            //Console.WriteLine($"Your grade is {grade}, you got a C.");
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            //Console.WriteLine($"Your entered grade is {grade}, you got a D.");
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            //Console.WriteLine($"Your entered grade is {grade}, you got an F.");
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+        if (grade > 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass the class, please contact your instructor.");
+        }
     }
 }
