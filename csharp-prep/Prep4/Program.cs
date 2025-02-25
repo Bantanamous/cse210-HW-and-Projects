a66fd1b7c060d3b589a1483d7183f8644b6ddd16
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-02-25 12:47:10 -0700

    PREP4:
    Added functionality to also now show which number provided is the largest and the smallest.
    
    Fixed a problem with the part that took the average to now properly work.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -10,6 +10,8 @@ class Program
         int sum = 0;
         int average = 0;
         int count = 0;
+        int largest = 0;
+        int smallest = 100000000;
         
         Console.WriteLine("Enter a list of numbers, type '0' to finish.");
         
@@ -30,13 +32,25 @@ class Program
             Console.WriteLine();
             Console.Write($"{number}");
             //Console.WriteLine();
-            count =+ 1 ;
+            count = count + 1;
             sum = sum + number;
             average = sum / count;
+            if (number > largest)
+            {
+                largest = number;
+            }
+            if (number < smallest)
+            {
+                smallest = number;
+            }
         }
         Console.WriteLine();
         Console.Write($"Sum: {sum}");
         Console.WriteLine();
         Console.Write($"Average: {average}");
+        Console.WriteLine();
+        Console.Write($"Largest: {largest}");
+        Console.WriteLine();
+        Console.Write($"Smallest: {smallest}");
     }
 }
