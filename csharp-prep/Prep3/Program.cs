6d49c66cbcb14fc3d6e26869b0698aab103717cd
Shaymon Madrid <167814619+Bantanamous@users.noreply.github.com>
2025-02-24 21:09:23 -0700

    PREP 3:
    Finished the core requirements, initially the program asked a user for a "magic number" which they then tried to guess.
    This was later replaced with a random number generator that gets a number between 1 and 100.
    The program uses a do-while loop to keep the user playing until they guess the magic number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.ComponentModel;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int intMagicNumber;
+        int intUserGuess;
+        
+        // Asked the user for a magic number, now uses a random number from the computer.
+        /* Console.WriteLine("What is the Magic Number?");
+        string magicNumber = Console.ReadLine();
+        intMagicNumber = int.Parse(magicNumber); */
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        // A do while loop to keep the user playing until the magic number is correctly guessed.
+        do 
+        {            
+            Console.WriteLine("What is your guess?");
+            string userGuess = Console.ReadLine();
+            intUserGuess = int.Parse(userGuess);
+             
+            if (number < intUserGuess)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else if (number > intUserGuess)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the magic number!");
+            } 
+
+        } while (number != intUserGuess);
+
+       
     }
 }
