b2dd5d7db5c0d152d80c33b2a8e5ae3a187bf4d7
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-02-25 12:37:06 -0700

    PREP4:
    Fulfilled the requirements:
    
    This program takes inputs from the user where we make a numerical list and give it back to the user. The sum and average of the list is also calculated and returned.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+        int intUserNumber = 1;
+        int sum = 0;
+        int average = 0;
+        int count = 0;
+        
+        Console.WriteLine("Enter a list of numbers, type '0' to finish.");
+        
+        do 
+            {
+                string userNumber = Console.ReadLine();
+                intUserNumber = int.Parse(userNumber);
+                
+                if (intUserNumber != 0)
+                {
+                    numbers.Add(intUserNumber);
+                }
+
+            } while (intUserNumber !=0);
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine();
+            Console.Write($"{number}");
+            //Console.WriteLine();
+            count =+ 1 ;
+            sum = sum + number;
+            average = sum / count;
+        }
+        Console.WriteLine();
+        Console.Write($"Sum: {sum}");
+        Console.WriteLine();
+        Console.Write($"Average: {average}");
     }
 }
