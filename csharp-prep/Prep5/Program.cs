71e2854bbde7b3b8c8782c4886b78384de0ace89
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-02-26 09:02:34 -0700

    PREP 5:
    This program, using functions, welcomes the user to the program and then asks for their name and a number.
    This number is then squared; the result is displayed with their name.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
-    static void Main(string[] args)
+    static void DisplayWelcome()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.Write("Welcome to the Program");
+    }
+
+    static string PromptUserName ()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string number = Console.ReadLine();
+        int intNumber = int.Parse(number);
+        return intNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        
+        Console.Write($"{name}, the square of your number is {square}.");
+    }
+    static void Main(string[] args)
+    {  
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+
     }
 }
