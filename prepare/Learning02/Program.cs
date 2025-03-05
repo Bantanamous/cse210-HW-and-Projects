9d056ab101af992813d8baa26debe59debf05e8c
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-03-05 09:58:14 -0700

    LEARNING 02
    Class variables are now private and constructors are now used. Also added a new method to the Resume class for adding jobs to the list.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,27 +6,29 @@ class Program
         Console.WriteLine();
         Console.WriteLine("Hello Learning02 World!");
 
-        Job job1 = new Job();
-        job1._company = "Lockheed Martin";
-        job1._jobTitle = "Quantity Assurance Officer";
-        job1._startYear = 2021;
-        job1._endYear = 2025;
+        Job job1 = new Job("Lockheed Martin","Quantity Assurance Officer",2021,2025);//now using constructor
+        // job1._company = "Lockheed Martin";
+        // job1._jobTitle = "Quantity Assurance Officer";
+        // job1._startYear = 2021;
+        // job1._endYear = 2025;
 
-        Job job2 = new Job();
-        job2._company = "Bro's Bread";
-        job2._jobTitle = "Master of Bread";
-        job2._startYear = 2005;
-        job2._endYear = 2027;
+        Job job2 = new Job("Bro's Bread","Master of Bread",2005,2027);//now using constructor
+        // job2._company = "Bro's Bread";
+        // job2._jobTitle = "Master of Bread";
+        // job2._startYear = 2005;
+        // job2._endYear = 2027;
 
         Console.WriteLine();
         job1.Display();
         job2.Display();
         Console.WriteLine();
         
-        Resume Resume = new Resume();
-        Resume._name = "Geoffery Vaynerchuk";
-        Resume._jobs.Add(job1);
-        Resume._jobs.Add(job2);
+        Resume Resume = new Resume("Geoffery Vaynerchuk");//using constructor to add name at start
+        // Resume._name = "Geoffery Vaynerchuk";
+        Resume.AddJob(job1);
+        Resume.AddJob(job2);
+        // Resume._jobs.Add(job1);
+        // Resume._jobs.Add(job2);
 
         Resume.Display();
         Console.WriteLine();
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,14 +1,24 @@
 using System;
+using System.ComponentModel;
 
 class Resume
 {
-    public string _name;
-    public List<Job> _jobs = new List<Job>();
+    private string _name;
+    private List<Job> _jobs;// _jobs = new List<Job>();
 
+    public Resume(string name)
+    {
+        _name = name;
+        _jobs = new List<Job>();
+    }
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
     public void Display()
     {
         Console.WriteLine($"Name: {_name}");
-        Console.WriteLine("Jobs");
+        Console.WriteLine("Jobs:");
         // Console.WriteLine($"Job Title, Company, Start Year, End Year");
         foreach (Job job in _jobs)
         {
