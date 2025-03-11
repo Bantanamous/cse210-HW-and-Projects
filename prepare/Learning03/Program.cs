13b27d36cf00453e20bebeedd5d58720b73d64b3
Shaymon Madrid <167814619+Bantanamous@users.noreply.github.com>
2025-03-10 19:28:36 -0700

    Learning03:
    Added a Fraction Class file:
    - added attributes for numerator and denominator
    - added several constructors to initialize the attributes
    - added getters and setters
    -added a method to display as a fraction (string type)
    - added a method to display as a decimal (double type)
    Tested in the program.cs file under main

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+
+    // Fraction f1 = new Fraction();
+    // Fraction f2 = new Fraction(6);
+    // Fraction f3 = new Fraction(7,6);
+    // f1.SetTop(1);
+    // f1.SetBottom(2);
+    // Console.WriteLine(f1.GetTop() + "/" + f1.GetBottom());
+
+    Fraction fraction = new Fraction();
+    Fraction fraction1 = new Fraction(1);
+    Fraction fraction2 = new Fraction(1, 2);
+    Console.WriteLine(fraction.GetFractionString());
+    Console.WriteLine(fraction1.GetFractionString());
+    Console.WriteLine(fraction2.GetFractionString());
+
+    
+    fraction1.SetTop(15);
+    fraction2.SetTop(13);
+    fraction2.SetBottom(7);
+    fraction2.GetFractionString();
+    Console.WriteLine(fraction2.GetFractionString());
+    fraction2.GetDecimalValue();
+    Console.WriteLine(fraction2.GetDecimalValue());
+
     }
 }
