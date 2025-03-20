18e7a2da1e9cb547caadd9232fb50d1d2dcf9ecc
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-03-20 09:17:39 -0600

    SANDBOX spinner demo

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,11 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        // Console.WriteLine("Hello World!");
 
-        Circle myCircle = new Circle(10); //give in the radius r
-        myCircle.DisplayCircleArea();
-        Cylinder myCylinder = new Cylinder(10, myCircle); //give in the height, 10, and the circle of circle type 'myCirlce'
-        myCylinder.DisplayCylinderVolume();
+        // Circle myCircle = new Circle(10); //give in the radius r
+        // myCircle.DisplayCircleArea();
+        // Cylinder myCylinder = new Cylinder(10, myCircle); //give in the height, 10, and the circle of circle type 'myCirlce'
+        // myCylinder.DisplayCylinderVolume();
+
+        int counter = 0; //spinner demo
+        while(counter < 10)
+        {
+            Console.Write("+");
+            Console.Write("\b");
+            Thread.Sleep(1000);
+            Console.Write("-");
+            Console.Write("\b");
+            Thread.Sleep(1000);
+            counter++;
+        }
     }
 }
