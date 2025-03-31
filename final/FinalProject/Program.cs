4eb597f7983c67f84ec9772c2fb5ada8f2835c36
Bantanamous <167814619+Bantanamous@users.noreply.github.com>
2025-03-31 09:15:26 -0600

    Final update

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,22 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 public class Program
 {
-    public static async Task Main(string[] args)
+    public static void Main(string[] args)
     {
-        int issId = 25544;
-        double latitude = 40.7128;
-        double longitude = -74.0060;
-        double altitude = 10.0;
         
-        SatelliteInfo issInfo = await SatelliteTracker.GetSatelliteInfo(issId);
-        Console.WriteLine($"Satellite: {issInfo.GetName()}\nTLE1: {issInfo.GetTleLine1()}\nTLE2: {issInfo.GetTleLine2()}");
-        
-        PassPrediction nextPass = await SatelliteTracker.GetNextPass(issId, latitude, longitude, altitude);
-        Console.WriteLine($"Next pass starts at {nextPass.GetStartTime()} with azimuth {nextPass.GetStartAzimuth()}");
     }
 }
